f88ff5e7dbe5633d70dbd98834356c540f381801
JC <jordanaclarke@hotmail.co.uk>
2023-12-04 13:44:07 +0000

    User input one letter to be pushed to github

---
+++ hangman.py
@@ -0,0 +1,15 @@
+import random
+
+word_list = ["apple", "orange", "banana", "mango", "blueberry"] 
+print(word_list)
+
+word = random.choice(word_list)
+print(word)
+
+guess = input("Please enter a single character: ")
+print(guess)
+
+if len(guess) == 1 and guess.isalpha():
+    print ("Good guess!")
+else: 
+    print("Oops! That is not a valid input.")
